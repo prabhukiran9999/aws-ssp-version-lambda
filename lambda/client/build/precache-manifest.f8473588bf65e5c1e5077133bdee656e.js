17e612e7239b699e045d394e935ba1931d7bd3a2
prabhukiran9999 <prabhu.manchineella@MacBook-Pro.local>
2021-09-22 13:03:56 -0400

    added modified client

---
+++ lambda/client/build/precache-manifest.f8473588bf65e5c1e5077133bdee656e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6bc0b24e7d603449a6801149e7704c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e7fed56597bb7a4ab17",
+    "url": "/static/js/2.dff85a77.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.dff85a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e1f0a871dcc4c2a3ad",
+    "url": "/static/js/3.1bea64b7.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/static/js/3.1bea64b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea52a0bdd3951f6c3f01",
+    "url": "/static/js/4.865a58ff.chunk.js"
+  },
+  {
+    "revision": "1020d706ec509cdb531f",
+    "url": "/static/js/5.58539bf7.chunk.js"
+  },
+  {
+    "revision": "8aec8746580678217af3",
+    "url": "/static/js/main.3b6c8271.chunk.js"
+  },
+  {
+    "revision": "dc8f32e3f54eefb3df3f",
+    "url": "/static/js/runtime-main.2a4462e5.js"
+  },
+  {
+    "revision": "eb9532033c2adf99b1314611b5e9cd0e",
+    "url": "/static/media/Lato-Bold.eb953203.ttf"
+  },
+  {
+    "revision": "863b7dcd5ec2c3923122af25ce0f7e4c",
+    "url": "/static/media/Lato-Medium.863b7dcd.ttf"
+  },
+  {
+    "revision": "3b9b99039cc0a98dd50c3cbfac57ccb2",
+    "url": "/static/media/Lato-Regular.3b9b9903.ttf"
+  },
+  {
+    "revision": "dcfc3bbef6fbaea65dcb97108b331a5f",
+    "url": "/static/media/desktop-logo.dcfc3bbe.svg"
+  },
+  {
+    "revision": "dcfc3bbef6fbaea65dcb97108b331a5f",
+    "url": "/static/media/traveller-support-mobile-outlined-v2.dcfc3bbe.svg"
+  }
+]);
